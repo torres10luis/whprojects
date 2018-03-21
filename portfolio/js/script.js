eb81c82799e9ffa1f5cc8647c3fd9d6a2e8998b3
torres10luis <Luis.angel_15@yahoo.com>
2018-03-21 13:52:35 -0700

    portfolio js intergration

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,5 +1,5 @@
 // document.getElementById('bob').style.backgroundColor ="red";
 
-function bob(){
-    document.getElementById('main').style.backgroundColor = "blue";
-}
+// function bob(){
+//     document.getElementById('main').style.backgroundColor = "blue";
+// }
