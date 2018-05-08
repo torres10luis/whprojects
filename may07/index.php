ebb8b1e5dc88961388c10635548d5793edc80fd7
torres10luis <Luis.angel_15@yahoo.com>
2018-05-08 15:15:39 -0700

    Mamp

---
+++ may07/index.php
@@ -0,0 +1,56 @@
+<?php
+    /*
+    $first = 'matt';
+    $last = 'higley';
+    $a2 = 10;
+    $b = 25;
+    $c = $a2 + $b;
+    $d = 'black';
+    */
+
+
+
+?>
+
+ 
+<?php
+
+    $x = 5;
+    function test($myVar){
+        echo "the value of x is: " .$myVar;
+    }
+
+    test($x);
+
+    function test2(){
+       global $y;
+        $y = 75;
+    }
+    test2();
+?>
+
+ <!-- <p>Hello  
+ <?php // $txt = $first , ' ', $last; print $txt; ?>, Welcome to my site.
+ 
+ </p>
+
+ <p>My car is <?php //echo $d; ?> </p>
+
+ <p>the answer is <?php //echo $c; ?> </p>
+ 
+ <p>
+        /* <?php 
+            //print 'Test'; 
+            
+         //block mute
+        
+            
+            
+           // $x = 5;
+           // echo $x;
+
+        
+        ?> */
+ </p> 
+
+ --> 
