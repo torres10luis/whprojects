c478914edaac4de38ee3caf5ad82872b735db232
torres10luis <Luis.angel_15@yahoo.com>
2018-05-11 13:56:02 -0700

    angular class 2

---
+++ may11/js/script.js
@@ -0,0 +1,46 @@
+// function addTwo(num){
+//     return num + 2;
+// }
+
+// console.log(addTwo(2));
+// //implisit function does a return like the one above
+// let addFour = num => num + 4;
+
+// console.log(addFour(2));
+/*
+function Counter(num){
+    this.num = num;
+    this.timer = setInterval( () =>{
+        this.num++;
+    }, 1000);
+}
+let num1 = new Counter(0);
+let num2 = new Counter(100);
+*/
+
+// let numbersPlusTwo= [];
+/*
+// for(let n=0; n< numbers.length; n++){
+//     numbersPlusTwo.push(numbers[n] + 2);
+// }
+numbers.forEach(x =>numbersPlusTwo.push(x+2));
+console.log(numbersPlusTwo)
+*/
+
+// let numbersPlusTwo = numbers.map( x => x + 2); 
+
+// console.log(numbers, numbersPlusTwo);
+
+// let numbersEven = numbers[Symbol.iterator]();
+// numbers.forEach(() => {
+//     if(!numbersEven.next().value % 2 ==0)
+//     console.log(numbersEven.next());
+
+// });
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+// let numbersEven = numbers.filter(x => x % 2 ==0);
+// console.log(numbersEven);
+
+
